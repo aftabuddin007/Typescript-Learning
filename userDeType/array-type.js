8dc29b4756d076e4b8e0e9bde800401584640ed4
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-05-03 23:11:48 +0600

    learning array

---
+++ userDeType/array-type.js
@@ -0,0 +1,9 @@
+"use strict";
+let userNames;
+userNames = ["aftab", "sajid", "hassan"];
+console.log(userNames[2]);
+userNames.sort();
+console.log(userNames);
+let multipleTypes;
+multipleTypes = ["aftab", 1, "sajid", 2, "hassan", 3];
+console.log(multipleTypes[3]);
---
+++ userDeType/array-type.ts
@@ -0,0 +1,12 @@
+
+let userNames:string[];
+
+
+userNames = ["aftab", "sajid", "hassan"];
+console.log(userNames[2]);
+userNames.sort();
+console.log(userNames);
+
+let multipleTypes:(string|number)[]
+multipleTypes = ["aftab", 1, "sajid", 2, "hassan", 3];
+console.log(multipleTypes[3]);
