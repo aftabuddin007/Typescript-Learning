55c8deb1106e543743fb1a2f4ef7f71d99031b83
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-05-04 22:14:02 +0600

    learn object type

---
+++ userDeType/object-type.js
@@ -0,0 +1,9 @@
+"use strict";
+// let user1 = {userName:"asew",age:32}
+// console.log(user1);
+let users;
+users = [];
+let user1;
+user1 = { userName: "asew", age: 32 };
+users.push(user1);
+console.log(users);
---
+++ userDeType/object-type.ts
@@ -0,0 +1,8 @@
+// let user1 = {userName:"asew",age:32}
+// console.log(user1);
+let users:object[] 
+ users=[];
+let user1:{userName:string,age:number}
+user1 = {userName:"asew",age:32}
+users.push(user1)
+console.log(users);
